0c35dbf9baa7f719eb717e207ec2c59e6a95658e
natalienprn <nattnprn@gmail.com>
2023-11-06 13:17:50 +1300

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <HashRouter> */}
+    <HashRouter>
       <App /> 
-    {/* </HashRouter> */}
+    </HashRouter>
 
   </React.StrictMode>,
 )
