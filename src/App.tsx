4832da8a67c266df28c6aeb20c319a07cd57e5a1
natalienprn <nattnprn@gmail.com>
2023-11-06 13:08:56 +1300

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-// import { Route, Routes} from "react-router-dom"
-// import Home from "./pages/home"
-// import Info from "./pages/info"
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello</h1>
-      {/* <Routes>
-        <Route path='/react-ts-v' element = {<Home />} />
-        <Route path='/react-ts-v/info' element = {<Info />} />
-
-      </Routes> */}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
